190d6482676e153fddbbe978d5c5b1efab947bcb
nigelmalaba1 <123284219+nigelmalaba1@users.noreply.github.com>
2023-01-31 16:40:28 +0000

    test

--- breakfast.py
+++ breakfast.py
@@ -35,52 +35,37 @@ def breakfastfun():
 #create a function that returns a string with 2 breakfast items
 
 def norepeat(): 
-        return "Ok here is something new for you. Your breakfast is " + breakfast[2] + " and " + breakfast[3] + "."
+    return "Ok here is something new for you. Your breakfast is " + breakfast[2] + " and " + breakfast[3] + "."
 
 #create a route for the /tired path
 @app.route("/tired")
 
 #create a function that returns a string with 1 breakfast item and 1 beverage
-
 def caffein():
-         
-        return "Your breakfast is " + breakfast[4] + " and " + beverages[1] + " because you're tired"
-
+    return "Your breakfast is " + breakfast[4] + " and " + beverages[1] + " because you're tired"
 
 #create a route for the /<dayoftheweek> path
-
 @app.route("/<day>")
 
 #create a function that returns a string that says "You can eat anything you want today!" if it's Saturday, "Let's nourish up with some fruits before the start of the week" if it's Sunday, and "It's oatmeal today!"
-
 def weekend(day):
-    
-    #if(day == "Saturday"):
-          # return "You can eat anything you want today!"
-    #if(day == "Sunday"):
-       #return "Let's nourish up with some fruits before the start of the week. Here are some options for you:\n" + fruits[1] + " " + fruits[2] + " " + fruits[3] + " " + fruits[4] + " " + fruits[5] + " " + fruits[6]    
-    
-#loop through the days of the week and print out the day of the week and the breakfast item for that day of the week
-
-        if day == "Monday":
-            return "It's " + days[0] + ". Your breakfast is " + breakfast[0] + "."
-        
-        if day == "Tuesday":
-            return "It's " + days[1] + ". Your breakfast is " + breakfast[1] + "."
-        if day == "Wednesday":
-            return "It's " + days[2] + ". Your breakfast is " + breakfast[2] + "."
-        if day == "Thursday":
-            return "It's " + days[3] + ". Your breakfast is " + breakfast[3] + "."  
-        if day == "Friday":
-            return "It's " + days[4] + ". Your breakfast is " + breakfast[4] + "."
-        if day == "Saturday":
-            return "It's " + days[5] + ". Your breakfast is " + breakfast[5] + "."
-        else:
-            return "It's oatmeal today! You didn't specify what you want so I'm giving you oatmeal."
-
-
+    #loop through the days of the week and print out the day of the week and the breakfast item for that day of the week
+    if day == "Monday":
+        return "It's " + days[0] + ". Your breakfast is " + breakfast[0] + "."
+    if day == "Tuesday":
+        return "It's " + days[1] + ". Your breakfast is " + breakfast[1] + "."
+    if day == "Wednesday":
+        return "It's " + days[2] + ". Your breakfast is " + breakfast[2] + "."
+    if day == "Thursday":
+        return "It's " + days[3] + ". Your breakfast is " + breakfast[3] + "."  
+    if day == "Friday":
+        return "It's " + days[4] + ". Your breakfast is " + breakfast[4] + "."
+    if day == "Saturday":
+        return "It's " + days[5] + ". Your breakfast is " + breakfast[5] + "."
+    else:
+        return "It's oatmeal today! You didn't specify what you want so I'm giving you oatmeal."
 
 if __name__ == "__main__":
    # server = wsgiserver.WSGIServer(breakfast, port=5000, numthreads=2)
     #server.start()
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
