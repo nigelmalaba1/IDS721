9a04c57bdcacff83ddcfafad3582dea82219eaa9
Nigel Malaba <123284219+nigelmalaba1@users.noreply.github.com>
2023-01-31 23:21:36 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import unittest
-from flask import Flask
 
-class FlaskMicroserviceTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = Flask(__name__)
-        self.client = self.app.test_client()
-
-    def test_index(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello, World!', response.data)
-
-if __name__ == '__main__':
-    unittest.main()
